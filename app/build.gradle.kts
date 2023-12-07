14b0281c73a11b0f97c92b667a05c97a1a81bf8a
benedikt.schmatz(at)gmail.com <benedikt.schmatz@edu.fh-joanneum.at>
2023-12-07 20:23:02 +0100

    Add inputs and save button to profile screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,7 +55,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
+    implementation("androidx.compose.material3:material3-window-size-class:1.1.2")
     implementation ("androidx.navigation:navigation-compose:2.7.0-rc01")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -64,4 +65,5 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
 }
--- app/src/main/java/com/example/hydrinker/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/hydrinker/screens/ProfileScreen.kt
@@ -1,23 +1,65 @@
 package com.example.hydrinker.screens
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import android.content.Context
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun ProfileScreen(navController: NavController) {
+fun ProfileScreen(navController: NavController, context: Context = LocalContext.current) {
+    var name by remember { mutableStateOf(TextFieldValue("")) }
+    var weight by remember { mutableStateOf(TextFieldValue("")) }
+    var dailyGoal by remember { mutableStateOf(TextFieldValue("")) }
+    var age by remember { mutableStateOf(TextFieldValue("")) }
+    var drinkSize by remember { mutableStateOf(TextFieldValue("")) }
+
+    var isNameValid by remember { mutableStateOf(true) }
+    var isWeightValid by remember { mutableStateOf(true) }
+    var isAgeValid by remember { mutableStateOf(true) }
+    var isDailyGoalValid by remember { mutableStateOf(true) }
+    var isDrinkSizeValid by remember { mutableStateOf(true) }
+
+    fun validateFields(name: String, weight: String, age: String, dailyGoal: String, drinkSize: String): Boolean {
+        isNameValid = name.isNotEmpty()
+        isWeightValid = weight.isNotEmpty() && weight.toDoubleOrNull() != null
+        isAgeValid = age.isNotEmpty() && age.toIntOrNull() != null
+        isDailyGoalValid = dailyGoal.isNotEmpty() && dailyGoal.toDoubleOrNull() != null
+        isDrinkSizeValid = drinkSize.isNotEmpty() && drinkSize.toDoubleOrNull() != null
+
+        return isNameValid && isWeightValid && isAgeValid && isDailyGoalValid && isDrinkSizeValid
+    }
+
+    fun saveUser(
+        context: Context,
+        name: String,
+        weight: String,
+        age: String,
+        dailyGoal: String,
+        drinkSize: String
+    ) {
+        val sharedPreferences = context.getSharedPreferences("UserPreferences", Context.MODE_PRIVATE)
+        with(sharedPreferences.edit()) {
+            putString("Name", name)
+            putString("Weight", weight)
+            putString("Age", age)
+            putString("DailyGoal", dailyGoal)
+            putString("DrinkSize", drinkSize)
+            apply()
+        }
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -25,19 +67,87 @@ fun ProfileScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Top
     ) {
-        Box(
+        OutlinedTextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") },
+            singleLine = true,
+            isError = isNameValid,
             modifier = Modifier
                 .fillMaxWidth()
-                .height(200.dp)
-                .padding(horizontal = 15.dp, vertical = 10.dp)
-                .clip(MaterialTheme.shapes.large)
-        ) {
-
-        }
-        Text(
-            "Profile Screen",
-            style = MaterialTheme.typography.titleLarge,
-            modifier = Modifier.padding(vertical = 20.dp)
+                .padding(vertical = 8.dp)
         )
+        OutlinedTextField(
+            value = weight,
+            onValueChange = { weight = it },
+            label = { Text("Weight") },
+            trailingIcon = { Text("kg", style = MaterialTheme.typography.bodySmall) },
+            singleLine = true,
+            isError = isWeightValid,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        OutlinedTextField(
+            value = age,
+            onValueChange = { age = it },
+            label = { Text("Age") },
+            singleLine = true,
+            isError = isAgeValid,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        OutlinedTextField(
+            value = dailyGoal,
+            onValueChange = { dailyGoal = it },
+            label = { Text("Daily Goal") },
+            trailingIcon = { Text("l", style = MaterialTheme.typography.bodySmall) },
+            singleLine = true,
+            isError = isDailyGoalValid,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        OutlinedTextField(
+            value = drinkSize,
+            onValueChange = { drinkSize = it },
+            label = { Text("Default Drink Size") },
+            trailingIcon = { Text("l", style = MaterialTheme.typography.bodySmall) },
+            singleLine = true,
+            isError = isDrinkSizeValid,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        )
+        Button(
+            onClick = {
+                val isValid = validateFields(name.text, weight.text, age.text, dailyGoal.text, drinkSize.text)
+                if (isValid)
+                 {
+                    saveUser(
+                        context,
+                        name.text,
+                        weight.text,
+                        age.text,
+                        dailyGoal.text,
+                        drinkSize.text
+                    )
+                    Toast.makeText(context, "Profile saved successfully", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(
+                        context,
+                        "Something went wrong while saving the user",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 8.dp)
+        ) {
+            Text("Save Profile")
+        }
     }
+
 }
