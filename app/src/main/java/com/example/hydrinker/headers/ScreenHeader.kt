54b68507ca7640e8d63b5247644f6d7f86a09e26
Benedikt Schmatz <benedikt.schmatz@edu.fh-joanneum.at>
2023-12-09 18:53:13 +0100

    adjusted values for screenheader

--- app/src/main/java/com/example/hydrinker/headers/ScreenHeader.kt
+++ app/src/main/java/com/example/hydrinker/headers/ScreenHeader.kt
@@ -3,6 +3,7 @@ package com.example.hydrinker.headers
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -17,6 +18,7 @@ fun ScreenHeader(headerText: String) {
     Surface(
         modifier = Modifier
             .fillMaxWidth()
+            .padding(start = 48.dp, end = 48.dp)
             .height(50.dp),
         color = MaterialTheme.colorScheme.primary,
         shape = RoundedCornerShape(bottomEnd = 96.dp, bottomStart = 96.dp)
