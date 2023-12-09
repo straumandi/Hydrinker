c6ae0dfcccd36e683a7c9c6402e56a7acf58713a
Daniel_Zarnhofer <daniel.zarnhofer@edu.fh-joanneum.at>
2023-12-09 22:44:18 +0100

    customize buttons on home-screen v0.1 (non-functional)

--- app/src/main/java/com/example/hydrinker/screens/HomeScreen.kt
+++ app/src/main/java/com/example/hydrinker/screens/HomeScreen.kt
@@ -1,6 +1,11 @@
 package com.example.hydrinker.screens
 
+import android.webkit.WebSettings.TextSize
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -9,74 +14,109 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import com.example.hydrinker.R
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.hydrinker.ui.theme.HydrinkerTheme
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun HomeScreen(navController: NavController) {
+
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(horizontal = 16.dp, vertical = 32.dp)
+            .padding(horizontal = 15.dp, vertical = 15.dp)
     ) {
         // Profile button at the top left
-        IconButton(
-            onClick = { /* Go to Profile page */ },
-            modifier = Modifier
-                .size(48.dp)
-                .padding(8.dp)
+        Button(
+            modifier = Modifier.size(80.dp,80.dp),
+            border = BorderStroke(1.dp, color = Color(0xFF000000)),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 30.dp
+            ),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF98E0FF),
+                contentColor = Color(0xFF000000),
+            ),
+            shape = RoundedCornerShape(topEnd = 50.dp, topStart = 50.dp, bottomEnd = 50.dp, bottomStart = 50.dp),
+            onClick = { /*TODO*/ }
         ) {
-            Icon(
-                Icons.Filled.Person,
-                contentDescription = "Profile",
-                tint = MaterialTheme.colorScheme.primary
+            Image(
+                painter = painterResource(id = R.drawable.btn_home_profile),
+                contentDescription = "Testing",
+                modifier = Modifier.fillMaxSize()
             )
         }
 
         // Logo beneath the profile button
         Spacer(modifier = Modifier.height(16.dp))
+        Image(
+            painter = painterResource(id = R.drawable.btn_home_score), contentDescription = null)
         // Insert your logo here
 
         // Add Cup button at the bottom left
         Spacer(modifier = Modifier.weight(1f))
-        IconButton(
-            onClick = { /* Open cup settings */ },
-            modifier = Modifier
-                .size(48.dp)
-                .padding(8.dp)
+
+
+        //Cup standard button
+        Button(
+            modifier = Modifier.size(80.dp,80.dp),
+            border = BorderStroke(1.dp, color = Color(0xFF000000)),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 30.dp
+            ),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF98E0FF),
+                contentColor = Color(0xFF000000),
+            ),
+            shape = RoundedCornerShape(topEnd = 50.dp, topStart = 50.dp, bottomEnd = 50.dp, bottomStart = 50.dp),
+            onClick = { /*TODO*/ }
         ) {
-            Icon(
-                Icons.Filled.AddCircle,
-                contentDescription = "Add standard cup",
-                tint = MaterialTheme.colorScheme.primary
+            Image(
+                painter = painterResource(id = R.drawable.btn_home_addsrd),
+                contentDescription = "Testing",
+                modifier = Modifier.fillMaxSize()
             )
         }
 
-        // Score button at the bottom right
-        IconButton(
-            onClick = { /* Go to Score page */ },
-            modifier = Modifier
-                .size(48.dp)
-                .padding(8.dp)
+        //Cup custom button
+        Button(
+            modifier = Modifier.size(80.dp,80.dp),
+            border = BorderStroke(1.dp, color = Color(0xFF000000)),
+            elevation = ButtonDefaults.buttonElevation(
+                defaultElevation = 30.dp
+            ),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color(0xFF98E0FF),
+                contentColor = Color(0xFF000000),
+            ),
+            shape = RoundedCornerShape(topEnd = 50.dp, topStart = 50.dp, bottomEnd = 50.dp, bottomStart = 50.dp),
+            onClick = { /*TODO*/ }
         ) {
-            Icon(
-                Icons.Filled.Star,
-                contentDescription = "Score",
-                tint = MaterialTheme.colorScheme.primary
+            Image(
+                painter = painterResource(id = R.drawable.btn_home_addcstm),
+                contentDescription = "Testing",
+                modifier = Modifier.fillMaxSize()
             )
         }
+
     }
 }
